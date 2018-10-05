35b0609a30677d8c2b3f7023aefb4c0529bd3f5f
danny9mvp <danny9mvp@gmail.com>
2018-10-05 00:31:37 -0500

    Añadidas más personas.

--- src/main/java/com/reciclarteprototipo/rest/PersonaEndpoint.java
+++ src/main/java/com/reciclarteprototipo/rest/PersonaEndpoint.java
@@ -18,7 +18,17 @@ public class PersonaEndpoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Persona[] all() {
 		return new Persona[] {new Persona(1,"Daniel"),new Persona(2,"Janneth")
-				,new Persona(3,"Carolina"), new Persona(4, "Yhorman")};
+				,new Persona(3,"Carolina"), new Persona(4, "Yhorman")
+				, new Persona(5, "Mario"), new Persona(6, "Alex")
+				, new Persona(7, "Hyoandri"), new Persona(8, "MeMo")
+				, new Persona(9, "Malviajes"), new Persona(10, "Ingrid")
+				, new Persona(11, "Luisa"), new Persona(12, "Metacho")
+				, new Persona(13, "Cachondo"), new Persona(14, "Kevin")
+				, new Persona(15, "Fornesto"), new Persona(16, "Judas")
+				, new Persona(17, "Pereza mental"), new Persona(18, "Zorra")
+				, new Persona(19, "Nigga"), new Persona(20, "Borracho")
+				, new Persona(21, "Perea"), new Persona(22, "Gestor")
+				, new Persona(23, "Uribito")};
 	}
 	
 	@POST
